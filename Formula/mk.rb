a6d5d657c4f72a4a8e3fbb479e34789d1e177459
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-21 08:39:11 +0000

    weechat: update 2.0.1 bottle.

---
+++ Formula/mk.rb
@@ -0,0 +1,40 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Mk < Formula
+  desc "A mac tool for the job"
+  homepage "https://github.com/iOS-examples-Swift/MK"
+  url "https://github.com/iOS-examples-Swift/MK.git"
+  version "1.0.0"
+  sha256 "2fa15561e7e6070e87f63117a92d6c15f74427c421bc86add78298cf6be9998f"
+
+  # depends_on "cmake" => :build
+  depends_on :xcode => ["9.0", :build]
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    bin.install "MK"
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test mk`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
